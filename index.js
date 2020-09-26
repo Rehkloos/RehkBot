cd60d00f1bf9bb95dd10f4f46e4fd4435ed8030a
Andre Saddler <dresadd09691@gmail.com>
2020-09-25 20:09:18 -0400

    index: increase events emitter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const express = require("express");
 require('dotenv').config();
-require('events').EventEmitter.defaultMaxListeners = 15;
+require('events').EventEmitter.defaultMaxListeners = 20;
 
 
 // File imports
