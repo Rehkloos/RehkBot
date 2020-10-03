14670d74a2f7a3d3c8565342084d78b9cdc13b77
Andre Saddler <dresadd09691@gmail.com>
2020-10-03 03:15:56 -0400

    add static icon

--- cmds/commands/about.js
+++ cmds/commands/about.js
@@ -1,5 +1,7 @@
 const Commando = require('discord.js-commando');
-const { MessageEmbed } = require('discord.js');
+const {
+  MessageEmbed
+} = require('discord.js');
 
 module.exports = class AboutCommand extends Commando.Command {
   constructor(client) {
@@ -13,29 +15,28 @@ module.exports = class AboutCommand extends Commando.Command {
 
   async run(message) {
     message.channel.send(new MessageEmbed()
-    .setColor(0xDC143C)
-    .setTitle("About Bot")
-    .setDescription(
+      .setColor(0xDC143C)
+      .setTitle("About Bot")
+      .setDescription(
         "There are many commands with Among Us Bot. Most of them you don't even have to do anything to prep!\nBy using the `$commands` command you can view all of the commands."
-    )
-    .addFields({
+      )
+      .addFields({
         name: "Github",
         value: "[Contribute Here!](https://github.com/Rehkloos/queue-bot)",
         inline: "true",
-    }, {
+      }, {
         name: "Twitter",
         value: "[Follow me on Twitter!](https://www.twitter.com/rehkloos)",
         inline: "true",
-    }, {
+      }, {
         name: "Invite",
         value: "[Add ImposterBot to server](https://discord.com/api/oauth2/authorize?client_id=349236583611891714&permissions=1371630673&scope=bot)", // im planning on making this public
         inline: "true",
-    })
-    .setThumbnail(
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.HCuyxDU5qqVDlpp0FnPVJwAAAA%26pid%3DApi&f=1"
-    )
-    .setTimestamp()
-    .setFooter(
+      })
+      .attachFiles(['./static/icon.png'])
+      .setThumbnail('attachment://icon.png')
+      .setTimestamp()
+      .setFooter(
         `Requested by ${message.author.username}`,
         message.author.avatarURL)
     );
--- cmds/commands/maps.js
+++ cmds/commands/maps.js
@@ -1,5 +1,7 @@
 const Commando = require('discord.js-commando');
-const { MessageEmbed } = require('discord.js');
+const {
+  MessageEmbed
+} = require('discord.js');
 
 module.exports = class MapsCommand extends Commando.Command {
   constructor(client) {
@@ -13,24 +15,25 @@ module.exports = class MapsCommand extends Commando.Command {
 
   async run(message) {
     message.channel.send(new MessageEmbed()
-    .setTitle('All the Among Us maps')
-            .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.HCuyxDU5qqVDlpp0FnPVJwAAAA%26pid%3DApi&f=1')
-            .setFooter(`Requested by ${message.author.username}`,
-                message.author.avatarURL)
-            .setColor(0xDC143C)
-            .addFields({
-                name: 'Skeled',
-                value: 'https://www.fanbyte.com/wp-content/uploads/2020/08/xAmong-Us-Skeled-Vents.jpg,qx96128.pagespeed.ic.UQgyhfawhK.jpg',
-                inline: 'true'
-            }, {
-                name: 'Mira',
-                value: 'https://www.fanbyte.com/wp-content/uploads/2020/08/Among-Us-MIRA-UQ-Vents.jpg',
-                inline: 'true'
-            }, {
-                name: 'Polas',
-                value: 'https://www.fanbyte.com/wp-content/uploads/2020/09/xAmong-Us-Polas-Map-Vents.jpg,qx96128.pagespeed.ic.V7Tg_KkLi6.jpg',
-                inline: 'true'
-            }, )
+      .setTitle('All the Among Us maps')
+      .attachFiles(['./static/icon.png'])
+      .setThumbnail('attachment://icon.png')
+      .setFooter(`Requested by ${message.author.username}`,
+        message.author.avatarURL)
+      .setColor(0xDC143C)
+      .addFields({
+        name: 'Skeled',
+        value: 'https://www.fanbyte.com/wp-content/uploads/2020/08/xAmong-Us-Skeled-Vents.jpg,qx96128.pagespeed.ic.UQgyhfawhK.jpg',
+        inline: 'true'
+      }, {
+        name: 'Mira',
+        value: 'https://www.fanbyte.com/wp-content/uploads/2020/08/Among-Us-MIRA-UQ-Vents.jpg',
+        inline: 'true'
+      }, {
+        name: 'Polas',
+        value: 'https://www.fanbyte.com/wp-content/uploads/2020/09/xAmong-Us-Polas-Map-Vents.jpg,qx96128.pagespeed.ic.V7Tg_KkLi6.jpg',
+        inline: 'true'
+      }, )
     );
   }
 }
